c2f292dd4ec9a7eb2f69917000b5d2828df0e282
tsekhov88 <52081547+tsekhov88@users.noreply.github.com>
2019-07-06 17:31:26 +0300

    Delete Счетчик прошло дней.php

--- Счетчик прошло дней.php
+++
@@ -1,8 +0,0 @@
-<?php
-        $data = '01.01.2020';
-        $time = strtotime($data);
-        $today = time();
-        $day = ($today - $time)/86400*-1;
-        $day = floor($day);
-        echo "До нового года осталось ".$day." дней.";
-?>
